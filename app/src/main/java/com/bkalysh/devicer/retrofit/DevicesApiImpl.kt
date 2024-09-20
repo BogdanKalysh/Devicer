1b6699552a1c975bb158031b87218d4f71c27723
bodyaka <bogdankalysh20@gmail.com>
2024-09-20 15:27:42 +0300

    Changed server address and route for receiving devices data.

--- app/src/main/java/com/bkalysh/devicer/retrofit/DevicesApiImpl.kt
+++ app/src/main/java/com/bkalysh/devicer/retrofit/DevicesApiImpl.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface DevicesApiImpl : DevicesApi {
-    @GET("/test_android/items.test")
+    @GET("/devicer_backend/devices.json")
     override suspend fun getDevices(): Response<DevicesResponse>
 }
