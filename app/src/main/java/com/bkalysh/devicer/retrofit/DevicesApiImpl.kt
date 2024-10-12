0d5d3f7b4705744b90122a63175d05a1755c7fc4
bodyaka <bogdankalysh20@gmail.com>
2024-10-12 19:17:28 +0300

    updated server route

--- app/src/main/java/com/bkalysh/devicer/retrofit/DevicesApiImpl.kt
+++ app/src/main/java/com/bkalysh/devicer/retrofit/DevicesApiImpl.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface DevicesApiImpl : DevicesApi {
-    @GET("/devicer_backend/devices.json")
+    @GET("/devicer_backend_static/devices.json")
     override suspend fun getDevices(): Response<DevicesResponse>
 }
